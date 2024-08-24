bb45d4447f340931730da5ed9462dd37917ff0e9
Ivan Shashero <ishashero@hystax.com>
2024-08-24 22:50:18 +0400

    Switched to single-message app with only routes takes as command
    Also removed unnecessary steps
    Code is a bit spaghetti, need to clean it up

--- src/announceman/main.py
+++ src/announceman/main.py
@@ -14,9 +14,7 @@ from aiogram.filters import Command, CommandStart, CommandObject
 from aiogram.fsm.context import FSMContext
 from aiogram.fsm.state import State, StatesGroup
 from aiogram.types import (
-    KeyboardButton,
     Message,
-    ReplyKeyboardMarkup,
     ReplyKeyboardRemove, InlineKeyboardMarkup, InlineKeyboardButton,
     LinkPreviewOptions, CallbackQuery,
 )
@@ -29,6 +27,8 @@ ROUTE_LIST_PAGE_LEN = 10
 ROUTES_PATH = "announceman_data/routes.json"
 ROUTES_CACHE = "announceman_data/.routes_loaded.pickle"
 START_POINTS_PATH = "announceman_data/starting_points.json"
+GO_BACK_DATA = "gobackdata"
+RESTART_DATA = "restartdata"
 
 routes = []
 start_points = {}
@@ -46,16 +46,12 @@ class StartPoint:
 
 
 class Form(StatesGroup):
-    date = State() # today, tomorrow or custom
-    time = State()  # presets or custom
-    track = State()  # initially - only custom
-    title = State()  # take name from the track or ask for custom text from the user
-    start_point = State()  # initially - only custom
-    summary = State()  # some base info
-    pace = State() # have presets for it
-    is_it_a_drop_ride = State() # drop/nodrop
-    post_scriptum = State() # extra info at the end
-    # lang?
+    date = State()
+    time = State()
+    track = State()
+    start_point = State()
+    pace = State()
+    announcement = State()
     # also add contact info of the user
 
 
@@ -64,13 +60,18 @@ async def command_start(message: Message, state: FSMContext) -> None:
     await state.clear()
     await state.set_state(Form.date)
 
-    await message.answer(
-        "Pick a date",
-        reply_markup=ReplyKeyboardMarkup(
-            keyboard=[[KeyboardButton(text="Today"), KeyboardButton(text="Tomorrow")]],
-            one_time_keyboard=True,
-            input_field_placeholder="Input your custom date here...",
-        )
+    await message.reply(
+        "Pick a date\n\nOr reply with a custom one",
+        parse_mode=ParseMode.MARKDOWN,
+        link_preview_options=LinkPreviewOptions(is_disabled=True),
+        reply_markup=InlineKeyboardMarkup(inline_keyboard=[
+            [
+                InlineKeyboardButton(text="Today", callback_data=datetime.now().strftime("%B %d")),
+                InlineKeyboardButton(
+                    text="Tomorrow", callback_data=(datetime.now() + timedelta(days=1)).strftime("%B %d"))
+            ],
+            [InlineKeyboardButton(text="Restart", callback_data=RESTART_DATA)],
+        ])
     )
 
 
@@ -86,179 +87,144 @@ async def cancel_handler(message: Message, state: FSMContext) -> None:
 
     logging.info("Cancelling state %r", current_state)
     await state.clear()
-    await message.answer(
-        "Cancelled.",
-        reply_markup=ReplyKeyboardRemove(),
-    )
-
-
-@form_router.message(Form.date)
-async def process_date(message: Message, state: FSMContext) -> None:
-    if message.text == "Today":
-        date = datetime.now().strftime("%B %d")
-    elif message.text == "Tomorrow":
-        date = (datetime.now() + timedelta(days=1)).strftime("%B %d")
-    else:
-        date = message.text
-
-    await state.update_data(date=date)
-    await state.set_state(Form.time)
-    await message.answer(
-        "Pick a time",
-        reply_markup=ReplyKeyboardMarkup(
-            keyboard=[
-                [
-                    KeyboardButton(text="07:00"),
-                    KeyboardButton(text="07:30"),
-                    KeyboardButton(text="08:00"),
-                ]
-            ],
-            resize_keyboard=True,
-            one_time_keyboard=True,
-            input_field_placeholder="Input your custom time here...",
-        ),
-    )
+    await message.answer("Cancelled.", reply_markup=ReplyKeyboardRemove())
 
 
 @form_router.callback_query()
 async def callback_query_handler(callback_query: CallbackQuery, state: FSMContext) -> None:
-    if await state.get_state() != Form.track:
+    callback_data = callback_query.data
+    form_state = await state.get_state()
+    stack = (await state.get_data()).get('stack', [])
+    if callback_data == RESTART_DATA:
+        return await command_start(callback_query.message, state)
+    elif callback_data == GO_BACK_DATA:
+        try:
+            stack.pop()
+            form_state_name, callback_data = stack.pop()
+        except IndexError:
+            return await command_start(callback_query.message, state)
+    else:
+        form_state_name = str(form_state)
+
+    if form_state_name != Form.track:
+        stack.append((form_state_name, callback_data))
+
+    async def show_route_list(offset):
+        route_previews = [f"{route.preview_message} --> /route\_{i}" for i, route in enumerate(routes)]
+        offset = int(offset) * ROUTE_LIST_PAGE_LEN
+        await callback_query.message.edit_text(
+            "\n".join(route_previews[offset:offset + ROUTE_LIST_PAGE_LEN]),
+            reply_markup=InlineKeyboardMarkup(inline_keyboard=[
+                [
+                    InlineKeyboardButton(text=str(i), callback_data=str(i))
+                    for i in range(len(route_previews) // ROUTE_LIST_PAGE_LEN + 1)
+                ],
+                [
+                    InlineKeyboardButton(text="Go back", callback_data=GO_BACK_DATA),
+                    InlineKeyboardButton(text="Restart", callback_data=RESTART_DATA),
+                ]
+            ])
+        )
+
+    if form_state_name == Form.date:
+        await state.update_data(date=callback_data, stack=stack)
+        await state.set_state(Form.time)
+
+        await callback_query.message.edit_text(
+            "Pick a time",
+            reply_markup=InlineKeyboardMarkup(
+                inline_keyboard=[
+                    [
+                        InlineKeyboardButton(text=start_time, callback_data=start_time)
+                        for start_time in ['07:00', '07:30', '08:00', '08:30', '09:00']
+                    ],
+                    [
+                        InlineKeyboardButton(text="Go back", callback_data=GO_BACK_DATA),
+                        InlineKeyboardButton(text="Restart", callback_data=RESTART_DATA),
+                    ]
+                ],
+            ),
+        )
+    elif form_state_name == Form.time:
+        await state.update_data(time=callback_data, stack=stack)
+        await state.set_state(Form.track)
+        await show_route_list(0)
+    elif form_state_name == Form.track:
+        if callback_data.startswith('/route_'):
+            await process_track_data(callback_data, callback_query.message, state)
+        else:
+            try:
+                await show_route_list(callback_data)
+            except Exception as e:
+                print(e)
+
+    elif form_state_name == Form.start_point:
+        sp = start_points.get(callback_data)
+        sp = sp.formatted if sp else callback_data
+        await state.update_data(start_point=sp, stack=stack)
+        await state.set_state(Form.pace)
+
+        await callback_query.message.edit_text(
+            "Define a pace",
+            reply_markup=InlineKeyboardMarkup(
+                inline_keyboard=[
+                    [
+                        InlineKeyboardButton(text="Skip", callback_data="skip"),
+                        InlineKeyboardButton(text="Easy", callback_data="easy/recovery"),
+                        InlineKeyboardButton(text="Z2", callback_data="Z2"),
+                        InlineKeyboardButton(text="Fast", callback_data="FAST"),
+                    ],
+                    [
+                        InlineKeyboardButton(text="Go back", callback_data=GO_BACK_DATA),
+                        InlineKeyboardButton(text="Restart", callback_data=RESTART_DATA),
+                    ]
+                ],
+            ),
+        )
+    elif form_state_name == Form.pace:
+        data = await state.get_data()
+        data['pace'] = callback_data
+
+        await callback_query.message.reply(
+            f"Announcement ({data['date']})\n\n"
+            f"{data['track']}\n"
+            f"{data['time']} at {data['start_point']}\n"
+            f"Pace: {data['pace']}",
+            reply_markup=ReplyKeyboardRemove(),
+        )
+        await state.clear()
+
+
+async def process_track_data(track_command, message: Message, state: FSMContext) -> None:
+    if not track_command.startswith('/route_'):
         return
 
-    route_previews = [f"{route.preview_message} --> /route\_{i}" for i, route in
-                      enumerate(routes)]
-    offset = int(callback_query.data) * ROUTE_LIST_PAGE_LEN
-    await callback_query.message.edit_text(
-        "\n".join(route_previews[offset:offset + ROUTE_LIST_PAGE_LEN]),
-        reply_markup=InlineKeyboardMarkup(inline_keyboard=[[
-            InlineKeyboardButton(text=str(i), callback_data=str(i)) for i in
-            range(len(route_previews) // ROUTE_LIST_PAGE_LEN + 1)
-        ]])
-    )
+    idx = int(track_command.split('_')[1])
+    route = routes[idx]
 
-
-@form_router.message(Form.time)
-async def process_time(message: Message, state: FSMContext) -> None:
-    await state.update_data(time=message.text)
-    await state.set_state(Form.track)
-
-    route_previews = [f"{route.preview_message} --> /route\_{i}" for i, route in enumerate(routes)]
-
+    data = await state.get_data()
+    data['stack'].append((str(await state.get_state()), track_command))
+    await state.update_data(track=route.preview_message, route=route, stack=data['stack'])
+    await state.set_state(Form.start_point)
     await message.reply(
-        "\n".join(route_previews[:ROUTE_LIST_PAGE_LEN]),
-        parse_mode=ParseMode.MARKDOWN,
-        link_preview_options=LinkPreviewOptions(is_disabled=True),
-        reply_markup=InlineKeyboardMarkup(inline_keyboard=[[
-            InlineKeyboardButton(text=str(i), callback_data=str(i)) for i in range(len(route_previews) // ROUTE_LIST_PAGE_LEN + 1)
-        ]])
+        "Choose a starting point",
+        reply_markup=InlineKeyboardMarkup(
+            inline_keyboard=[
+                [
+                    InlineKeyboardButton(text=sp, callback_data=sp) for sp in start_points
+                ],
+                [
+                    InlineKeyboardButton(text="Go back", callback_data=GO_BACK_DATA),
+                    InlineKeyboardButton(text="Restart", callback_data=RESTART_DATA),
+                ]
+            ],
+        ),
     )
 
 
 @form_router.message(Form.track)
 async def process_track(message: Message, state: FSMContext) -> None:
-    if message.text.startswith('/route_'):
-        idx = int(message.text.split('_')[1])
-        route = routes[idx]
-    else:
-        loop = asyncio.get_event_loop()
-        route = await loop.run_in_executor(None, load_route,message.text)
-
-    await state.update_data(track=route.preview_message, route=route)
-    await state.set_state(Form.title)
-
-    # todo: get name of the track from strava or komoot and use it as a preset for a button
-
-    await message.reply(
-        "Enter the title",
-        reply_markup=ReplyKeyboardRemove(),
-    )
-
-
-@form_router.message(Form.title)
-async def process_title(message: Message, state: FSMContext) -> None:
-    await state.update_data(title=message.text)
-    await state.set_state(Form.start_point)
-
-    await message.reply(
-        "Choose a starting point",
-        parse_mode=ParseMode.MARKDOWN,
-        link_preview_options=LinkPreviewOptions(is_disabled=True),
-        reply_markup=ReplyKeyboardMarkup(keyboard=[[
-            KeyboardButton(text=sp) for sp in start_points
-        ]])
-    )
-
-
-@form_router.message(Form.start_point)
-async def process_start_point(message: Message, state: FSMContext) -> None:
-    sp = start_points.get(message.text)
-    sp = sp.formatted if sp else message.text
-    await state.update_data(start_point=sp)
-    await state.set_state(Form.summary)
-
-    await message.reply(
-        "Describe your ride",
-        reply_markup=ReplyKeyboardRemove(),
-    )
-
-
-@form_router.message(Form.summary)
-async def process_summary(message: Message, state: FSMContext) -> None:
-    await state.update_data(summary=message.text)
-    await state.set_state(Form.pace)
-
-    await message.reply(
-        "Define the pace",
-        reply_markup=ReplyKeyboardRemove(),
-    )
-
-
-@form_router.message(Form.pace)
-async def process_pace(message: Message, state: FSMContext) -> None:
-    await state.update_data(pace=message.text)
-    await state.set_state(Form.is_it_a_drop_ride)
-
-    await message.reply(
-        "Is it a drop ride?",
-        reply_markup=ReplyKeyboardMarkup(
-            keyboard=[[KeyboardButton(text="No"), KeyboardButton(text="Yes")]],
-            resize_keyboard=True,
-            one_time_keyboard=True,
-        ),
-    )
-
-
-@form_router.message(Form.is_it_a_drop_ride)
-async def process_drop_ride(message: Message, state: FSMContext) -> None:
-    if message.text not in {"No", "Yes"}:
-        return
-
-    await state.update_data(drop_ride=message.text == "Yes")
-    await state.set_state(Form.post_scriptum)
-
-    await message.reply(
-        "Any extra comments you want to add?",
-        reply_markup=ReplyKeyboardRemove(),
-    )
-
-
-@form_router.message(Form.post_scriptum)
-async def process_post_scriptum(message: Message, state: FSMContext) -> None:
-    data = await state.update_data(post_scriptum=message.text)
-
-    await message.reply(
-        f"Announcement ({data['date']})\n"
-        f"{data['title']}\n"
-        f"{data['track']}\n"
-        f"{data['summary']}\n"
-        f"{data['start_point']} at {data['time']}\n\n"
-        f"{data['summary']}\n"
-        f"pace: {data['pace']} ({"No-drop ride" if data['drop_ride'] else "Drop ride"})\n\n"
-        f"{data['post_scriptum']}",
-        reply_markup=ReplyKeyboardRemove(),
-    )
-
-    await state.clear()
+    await process_track_data(message.text, message, state)
 
 
 @form_router.message(Command("preview"))
